cb3517950f2ae32b48322ba51abd09bd36de2878
Ahmad Buhamad <2142128368@cs.ku.edu.kw>
2018-10-21 20:20:47 +0300

    mess

--- test.c
+++ test.c
@@ -1,7 +1,9 @@
 int main(){
-	printf("test1");
-	printf("test21");
-	//added one line 1
-	//added one line 2
+	int x;
+	float j;
+	printf("test13333");
+	printf("test233331");
+	//added world
+	//hello
 	return 0;
 }
