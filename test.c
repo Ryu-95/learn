b4c233a79c013406fa8b575af8680664917b7c4f
Ahmad Buhamad <2142128368@cs.ku.edu.kw>
2018-10-21 20:28:42 +0300

    .

--- test.c
+++ test.c
@@ -5,12 +5,11 @@ int main(){
 	printf("1modify"); //mofified1
 	return 0;
 }
-<<<<<<< HEAD
 void test2(){
 	
-=======
+}
+
 int test2(){
 	int x;
 	return 1;
->>>>>>> 9378559d12f162e573755a7fb138259c86ae00dd
 }
